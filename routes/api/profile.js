fd5218a2b435fce3257e123021d7ffd98b6c70e0
Gnanavel <gnanaveln@gmail.com>
2019-06-30 23:22:16 +0100

    Create and update profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+//Load profile models
+
+const Profile = require('../../models/Profile');
+
+const User = require('../../models/User');
 
 // @route GET api/profile/test
 // @desc Tests profile route
@@ -8,5 +16,89 @@ const router = express.Router();
 router.get('/test', (req, res) => res.json({
     msg: "Profile Works"
 }));
+
+// @route GET api/profile
+// @desc get current user profile route
+// @access Private
+
+router.get('/', passport.authenticate('jwt', {
+    session: false
+}), (res, req) => {
+    const errors = {};
+    Profile.findOne({
+        user: req.user.id
+    }).then(profile => {
+        console.log('get request >>>> ', profile);
+        if (!profile) {
+            errors.noprofile = 'There is no profile for this user';
+            return res.statusCode(404).json();
+        }
+        res.json(profile);
+    }).catch(err => res.statusCode(404).json(err));
+});
+
+// @route POST api/profile
+// @desc create or edit user profile route
+// @access Private
+
+router.post('/', passport.authenticate('jwt', {
+    session: false
+}), (res, req) => {
+    const errors = {};
+
+    // Get fields
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.githubusername)
+        profileFields.githubusername = req.body.githubusername;
+    // Skills - Spilt into array
+    if (typeof req.body.skills !== 'undefined') {
+        profileFields.skills = req.body.skills.split(',');
+    }
+
+    // Social
+    profileFields.social = {};
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+    Profile.findOne({
+        user: req.user.id
+    }).then(profile => {
+        if (profile) {
+            // Update
+            Profile.findOneAndUpdate({
+                user: req.user.id
+            }, {
+                $set: profileFields
+            }, {
+                new: true
+            }).then(profile => res.json(profile));
+        } else {
+            // Create
+
+            // Check the handle exist
+            Profile.findOne({
+                handle: profileFields.handler
+            }).then(profile => {
+                if (profile) {
+                    errors.handle = 'that handle already exists';
+                    res.status(400).json(errors);
+                }
+
+                //Save profile
+                new Profile(profileFields).save().then(profile => res.json(profile));
+            })
+        }
+    })
+});
 
 module.exports = router;
